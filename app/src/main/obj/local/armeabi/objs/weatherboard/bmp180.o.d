bf5edf73ee4148ea5a3117d806eb459ffad699dc
Darren Pottinger <darren.pottinger@gmail.com>
2016-03-12 18:24:21 -0500

    Compiles static library and associated thumb.

---
+++ app/src/main/obj/local/armeabi/objs/weatherboard/bmp180.o.d
@@ -0,0 +1,5 @@
+/Users/darren/Developer/GitHub/dpott197/WeatherJni/app/src/main/obj/local/armeabi/objs/weatherboard/bmp180.o: \
+ /Users/darren/Developer/GitHub/dpott197/WeatherJni/app/src/main/jni/bmp180.c \
+ /Users/darren/Developer/GitHub/dpott197/WeatherJni/app/src/main/jni/bmp180.h
+
+/Users/darren/Developer/GitHub/dpott197/WeatherJni/app/src/main/jni/bmp180.h:
