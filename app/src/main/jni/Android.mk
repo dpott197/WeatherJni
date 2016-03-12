f83bd508bf4a6ebf75bf402f2c2eb13ccb2495c3
Darren Pottinger <darren.pottinger@gmail.com>
2016-03-12 18:22:21 -0500

    Adds Android.mk for idk-build.

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,21 @@
+LOG_TO_ANDROID_LOGCAT := true
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= weather_board.c, bme280.c, bme280-i2c.c, si1132.c, si702x.c, bmp180.c
+
+LOCAL_MODULE:=weatherboard
+
+LOCAL_CPPFLAGS+= --sysroot=$(SYSROOT)
+LOCAL_CFLAGS+= --sysroot=$(SYSROOT)
+LOCAL_CXXFLAGS+= --sysroot=$(SYSROOT)
+
+ifeq ($(LOG_TO_ANDROID_LOGCAT),true)
+LOCAL_CFLAGS+= -DDEBUG -DANDROID_NDK
+LOCAL_SHARED_LIBRARIES+= libcutils
+endif
+
+include $(BUILD_STATIC_LIBRARY)
+
+
