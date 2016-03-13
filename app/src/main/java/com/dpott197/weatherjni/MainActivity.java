649071195555ca243b9e5acca6ca09a470b6f02f
dpott197 <darren.pottinger@gmail.com>
2016-03-13 06:33:13 -0400

    Adds additional sensor methods.

--- app/src/main/java/com/dpott197/weatherjni/MainActivity.java
+++ app/src/main/java/com/dpott197/weatherjni/MainActivity.java
@@ -1,25 +1,45 @@
 package com.dpott197.weatherjni;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private TextView mAltitudeTextView;
+    private TextView mTempTextView;
+    private TextView mHumidityTextView;
+    private TextView mPressureTextView;
+    private TextView mVisibleTextView;
+
+    private CountDownTimer mRefreshTimer = new CountDownTimer(Integer.MAX_VALUE, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            updateUI();
+        }
+
+        @Override
+        public void onFinish() {
+            start();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         onCreateView();
+        mRefreshTimer.start();
     }
 
     private void onCreateView() {
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.activity_main_textView);
+
+        mAltitudeTextView = (TextView) findViewById(R.id.activity_main_altitudeTextView);
+        mHumidityTextView = (TextView) findViewById(R.id.activity_main_humidityTextView);
+        mPressureTextView = (TextView) findViewById(R.id.activity_main_pressureTextView);
+        mTempTextView = (TextView) findViewById(R.id.activity_main_tempTextView);
+        mVisibleTextView = (TextView) findViewById(R.id.activity_main_visibleTextView);
     }
 
     @Override
@@ -33,8 +53,27 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        if (mTextView != null) {
-            mTextView.setText(String.format(String.valueOf(getTemp())));
+        if (mAltitudeTextView != null) {
+            mAltitudeTextView.setText(String.format(String.valueOf(getAltitude())));
+        }
+
+        if (mHumidityTextView != null) {
+            mHumidityTextView.setText(String.format(String.valueOf(getHumidity())));
+        }
+
+        if (mPressureTextView != null) {
+            mPressureTextView.setText(String.format(String.valueOf(getPressure())));
+        }
+
+        if (mTempTextView != null) {
+            mTempTextView.setText(String.format(String.valueOf(getTemp())));
+        }
+
+        // FIXME
+        openLightDevice();
+
+        if (mVisibleTextView != null) {
+            mVisibleTextView.setText(String.format(String.valueOf(getVisible())));
         }
     }
 
@@ -43,6 +82,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public native int openDevice();
+    public native int openLightDevice();
+    public native double getAltitude();
+    public native double getHumidity();
+    public native double getPressure();
     public native double getTemp();
+    public native double getVisible();
 
 }
--- app/src/main/jni/weatherboard-jni.c
+++ app/src/main/jni/weatherboard-jni.c
@@ -10,14 +10,43 @@ static int temperature;
 static int humidity;
 static float altitude;
 
+JNIEXPORT jint JNICALL
+Java_com_dpott197_weatherjni_MainActivity_openDevice(JNIEnv *env, jobject instance) {
+    char *device = "/dev/i2c-1";
+    return bme280_begin(device);
+}
+
+JNIEXPORT jint JNICALL
+Java_com_dpott197_weatherjni_MainActivity_openLightDevice(JNIEnv *env, jobject instance) {
+    char *device = "/dev/i2c-1";
+    return si1132_begin(device);
+}
+
+JNIEXPORT jdouble JNICALL
+Java_com_dpott197_weatherjni_MainActivity_getAltitude(JNIEnv *env, jobject instance) {
+    bme280_read_pressure_temperature_humidity(&pressure, &temperature, &humidity);
+    return bme280_readAltitude(pressure, 1024.25);
+}
+
+JNIEXPORT jdouble JNICALL
+Java_com_dpott197_weatherjni_MainActivity_getHumidity(JNIEnv *env, jobject instance) {
+    bme280_read_pressure_temperature_humidity(&pressure, &temperature, &humidity);
+    return (double) humidity / 1024.0;
+}
+
+JNIEXPORT jdouble JNICALL
+Java_com_dpott197_weatherjni_MainActivity_getPressure(JNIEnv *env, jobject instance) {
+    bme280_read_pressure_temperature_humidity(&pressure, &temperature, &humidity);
+    return (double) pressure / 100.0;
+}
+
 JNIEXPORT jdouble JNICALL
 Java_com_dpott197_weatherjni_MainActivity_getTemp(JNIEnv *env, jobject instance) {
     bme280_read_pressure_temperature_humidity(&pressure, &temperature, &humidity);
     return (double) temperature / 100.0;
 }
 
-JNIEXPORT jint JNICALL
-Java_com_dpott197_weatherjni_MainActivity_openDevice(JNIEnv *env, jobject instance) {
-    char *device = "/dev/i2c-1";
-    return bme280_begin(device);
+JNIEXPORT jdouble JNICALL
+Java_com_dpott197_weatherjni_MainActivity_getVisible(JNIEnv *env, jobject instance) {
+    return Si1132_readVisible();
 }
